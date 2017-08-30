2b1fccc507aa085c9b5c3785de82226cc52d791d
n3m3sis42 <n3m3sis42@gmail.com>
2017-08-30 18:40:04 -0400

    built out scenes_controller to include create, index, update and destroy actions and confirmed it works with front end

--- app/controllers/api/v1/scenes_controller.rb
+++ app/controllers/api/v1/scenes_controller.rb
@@ -1,8 +1,36 @@
 class Api::V1::ScenesController < ApplicationController
 
+  def create
+    scene = Scene.create(scene_params)
+    render json: scene
+  end
+
   def index
-    scenes = Scene.all
+    # scenes = Scene.all
+    scenes = Scene.order("created_at DESC")
     render json: scenes
+  end
+
+  def update
+    scene = Scene.find(params[:id])
+    scene.update(scene_params)
+    render json: scene
+  end
+
+  def destroy
+    scene = Scene.find(params[:id])
+    if scene.destroy
+      head :no_content, status: :ok
+    else
+      render json: scene.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def scene_params
+    params.require(:scene).permit(:title, :notes)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :scenes, only: [:index]
+      resources :scenes, only: [:create, :index, :update, :destroy]
     end
   end
 
