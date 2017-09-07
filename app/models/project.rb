5fa25048e18a6fe2009c021fc3ec19d6b3877de1
n3m3sis42 <n3m3sis42@gmail.com>
2017-09-07 14:10:01 -0400

    added method on project model to take in an array with x, y coords and confirm whether they're already in use on the project

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,19 +11,31 @@
 
 class Project < ApplicationRecord
   has_many :scenes
+  has_many :positions, through: :scenes
   has_many :characters, through: :scenes
 
   def scene_positions
-    self.scenes.map{|scene| { id: scene_id, coords: [scene.x_coord, scene.y_coord] }}
+    self.scenes.map{|scene| { id: scene.id, coords: [scene.x_coord, scene.y_coord] }}
   end
 
   def build_api_data
     { project: self,
       scenes: {
         positions: self.scene_positions,
-        characters: self.scene.characters
+        characters: self.characters
       }
     }
   end
+
+  def position_in_use(coords)
+    self.scene_positions.find {|position| position[:coords] == coords}
+  end
+
+  # TODO
+  #
+  # write an instance method that returns the next available set of coords within the project (board? do i want a model for this?) to pass to the Draggable component on first render
+  #
+  # write another instance method that checks coords we are dragging to and returns an error (so we don't save the drag and card goes back to its previous location) if coords are in use
+
 
 end
